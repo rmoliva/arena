681315b398fd34f573defd7d3c77859d27bdb085
Roberto M. Oliva <roliva@lciberica.es>
2016-10-14 20:00:33 +0200

    Initial struture

---
+++ spec/index.spec.ts
@@ -0,0 +1,12 @@
+import * as test from 'tape';
+
+test('timing test', function (t: any) {
+    t.plan(2);
+
+    t.equal(typeof Date.now, 'function');
+    let start = Date.now();
+
+    setTimeout(function () {
+        t.equal(Date.now() - start, 100);
+    }, 100);
+});
