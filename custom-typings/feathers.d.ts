c294e49ff1098097f592ecfdfed8adc086ef109f
Roberto M. Oliva <roliva@lciberica.es>
2016-10-17 10:41:35 +0200

    feat(): Feathers MongoDB example
    Implemented the Feathers MongoDB example

--- custom-typings/feathers.d.ts
+++ custom-typings/feathers.d.ts
@@ -16,11 +16,31 @@ declare module 'feathers'{
 
 declare module 'feathers-mongodb'{
   let feathers_mongodb: {
-    (config: {
-      db: string;
-      collection: string;
-    }): any;
+    (config: any): any;
   };
 
   export = feathers_mongodb;
+}
+
+declare module 'feathers-rest'{
+  let feathers_rest: {
+    (): any;
+  };
+
+  export = feathers_rest;
+}
+
+declare module 'feathers-socketio'{
+  let feathers_socketio: {
+    (): any;
+  };
+  export = feathers_socketio;
+}
+
+declare module 'feathers-errors'{
+  let feathers_errors: {
+    handler: () => any;
+  };
+
+  export = feathers_errors;
 }
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,48 @@
 import * as feathers from 'feathers';
 import * as mongodb from 'feathers-mongodb';
-import * as bodyParser from'body-parser';
+import * as bodyParser from 'body-parser';
+import * as rest from 'feathers-rest';
+import * as socketio from 'feathers-socketio';
+// import * as errors from 'feathers-errors';
+import {MongoClient} from 'mongodb';
 
-let app = feathers();
 
-// On MongoDB use the `feathers-demo` database
-// and the `todos` collection
-let todoService = mongodb({
-  db: 'feathers-demo',
-  collection: 'todos'
+// Create a feathers instance.
+const app = feathers()
+  // Enable Socket.io
+  .configure(socketio())
+  // Enable REST services
+  .configure(rest())
+  // Turn on JSON parser for REST services
+  .use(bodyParser.json())
+  // Turn on URL-encoded parser for REST services
+  .use(bodyParser.urlencoded({extended: true}));
+
+
+const promise = new Promise(function(resolve) {
+  // Connect to your MongoDB instance(s)
+  MongoClient.connect('mongodb://localhost:27017/feathers').then(function(db){
+    // Connect to the db, create and register a Feathers service.
+    app.use('/messages', mongodb({
+      Model: db.collection('messages'),
+      paginate: {
+        default: 2,
+        max: 4
+      }
+    }));
+
+    // A basic error handler, just like Express
+    // app.use(errors.handler());
+
+    // Start the server
+    var server = app.listen(3030);
+    server.on('listening', function() {
+      console.log('Feathers Message MongoDB service running on 127.0.0.1:3030');
+      resolve(server);
+    });
+  }).catch(function(error){
+    console.error(error);
+  });
 });
 
-// Set up REST and SocketIO APIs
-app.configure(feathers.rest());
-app.configure(feathers.socketio());
-
-// Parse JSON and form HTTP bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Use the Todo service
-app.use('/todos', todoService);
-
-// Start the application on port 3030
-app.listen(3030);
+module.exports = promise;
